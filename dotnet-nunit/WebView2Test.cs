38c59577efe3e66b7b37fd8ac6a24f51de4984bf
Max Schmitt <max@schmitt.mx>
2022-09-26 10:51:18 +0200

    chore: dotnet cleanups

---
+++ dotnet-nunit/WebView2Test.cs
@@ -0,0 +1,61 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using System.Diagnostics;
+
+namespace dotnet_nunit;
+
+public class WebView2Test : PlaywrightTest
+{
+    public IBrowser Browser { get; internal set; } = null!;
+    public IBrowserContext Context { get; internal set; } = null!;
+    public IPage Page { get; internal set; } = null!;
+    private Process? _webView2Process = null;
+    private string _userDataDir = null!;
+    private string _executablePath = Path.Join(Directory.GetCurrentDirectory(), @"..\..\..\..\webview2-app\bin\Debug\net6.0-windows\webview2.exe");
+
+    [SetUp]
+    public async Task BrowserSetUp()
+    {
+        var cdpPort = 10000 + WorkerIndex;
+        Assert.IsTrue(File.Exists(_executablePath), "Make sure that the executable exists");
+        _userDataDir = Path.Join(Path.GetTempPath(), $"playwright-webview2-tests/user-data-dir-{TestContext.CurrentContext.WorkerId}");
+        // WebView2 does some lazy cleanups on shutdown so we can't clean it up after each test
+        if (Directory.Exists(_userDataDir))
+        {
+            Directory.Delete(_userDataDir, true);
+        }
+        _webView2Process = Process.Start(new ProcessStartInfo(_executablePath)
+        {
+            EnvironmentVariables =
+        {
+            ["WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS"] = $"--remote-debugging-port={cdpPort}",
+            ["WEBVIEW2_USER_DATA_FOLDER"] = _userDataDir,
+        },
+            RedirectStandardOutput = true,
+        });
+        while (!_webView2Process!.HasExited)
+        {
+            var output = await _webView2Process!.StandardOutput.ReadLineAsync();
+            if (_webView2Process!.HasExited)
+            {
+                throw new Exception("WebView2 process exited unexpectedly");
+            }
+            if (output != null && output.Contains("WebView2 initialized"))
+            {
+                break;
+            }
+        }
+        var cdpAddress = $"http://127.0.0.1:{cdpPort}";
+        Browser = await Playwright.Chromium.ConnectOverCDPAsync(cdpAddress);
+        Context = Browser.Contexts[0];
+        Page = Context.Pages[0];
+    }
+
+    [TearDown]
+    public async Task BrowserTearDown()
+    {
+        _webView2Process!.Kill(true);
+        await Browser.CloseAsync();
+    }
+}
